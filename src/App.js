d3418e73fa0fb1d4ce400e9a33dd4e4b36afbb23
novel <novelbafagih@student.ub.ac.id>
2021-06-19 10:21:29 +0700

    color changing button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
+import {FaQuoteLeft} from 'react-icons/fa';
+import {GrTwitter} from 'react-icons/fa';
 
 function App() {
   function createHsl(hue,lightness,saturation){
+    console.log(`hsl(${hue},${lightness}%,${saturation}%)`);
     return(`hsl(${hue},${lightness}%,${saturation}%)`);
   }
 
@@ -15,18 +17,18 @@ function App() {
     const currentHslColor = createHsl(currentHue,80,60);
 
     document.body.style.backgroundColor = currentHslColor;
-    setColor({color : currentHslColor});
     console.log(color);
+    setColor({color : currentHslColor});
   }
     
   return (
     <div id="wrapper">
       <div id="quote-box">
-        <p id="text" style={color}>Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.</p>
+        <p id="text" style={color}><FaQuoteLeft/> Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.</p>
         <p id="author" style={color}>-Steve jobs</p>
-
-        <button onClick={changingColor} id="new-quote">Press</button>
-
+        <div>
+          <button onClick={changingColor}  id="new-quote" style={{background: createHsl(currentHue,80,60)}}>New quote</button>
+        </div>
       </div>
     </div>
   );
