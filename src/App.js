0487807bf5cefc719aa1ee28e36529305f59ef28
novel <novelbafagih@student.ub.ac.id>
2021-06-19 11:10:27 +0700

    added tweet button and hover animation in tweet and new quote button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {useState} from 'react';
 import {FaQuoteLeft} from 'react-icons/fa';
-import {GrTwitter} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {TiSocialTumbler} from 'react-icons/ti';
 
 function App() {
   function createHsl(hue,lightness,saturation){
@@ -10,24 +11,28 @@ function App() {
   }
 
   const [currentHue, setHue] = useState(0);
-  const [color, setColor] = useState({color : 'hsl(0,80%,60%)'});
+  const [currentColor, setColor] = useState('hsl(0,80%,60%)');
 
   const changingColor = () => {
     currentHue === 250? setHue(0) : setHue(currentHue +50);
     const currentHslColor = createHsl(currentHue,80,60);
 
     document.body.style.backgroundColor = currentHslColor;
-    console.log(color);
-    setColor({color : currentHslColor});
+    console.log(currentColor);
+    setColor(currentHslColor);
   }
     
   return (
     <div id="wrapper">
       <div id="quote-box">
-        <p id="text" style={color}><FaQuoteLeft/> Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.</p>
-        <p id="author" style={color}>-Steve jobs</p>
-        <div>
-          <button onClick={changingColor}  id="new-quote" style={{background: createHsl(currentHue,80,60)}}>New quote</button>
+        <p id="text" style={{color: currentColor}}><FaQuoteLeft/> Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.</p>
+        <p id="author" style={{color: currentColor}}>-Steve jobs</p>
+        <div className="link">
+          <a id="tweet-quote" href="twitter.com/intent/tweet" style={{color: currentColor}}>
+            <FaTwitter/>
+          </a>
+
+          <button onClick={changingColor}  id="new-quote" style={{background: currentColor}}>New quote</button>
         </div>
       </div>
     </div>
