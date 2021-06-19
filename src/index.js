6a01199314beb93f0bf55d16b8e5c5d87a7137b8
novel <novelbafagih@student.ub.ac.id>
2021-06-19 19:56:42 +0700

    added readme file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
-import App from './App';
-import FetchRandomQuote from './fetchingApi';
+import App from './RandomQuotes';
 
 ReactDOM.render(
   <React.StrictMode>
