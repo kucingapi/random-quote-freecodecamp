976e2fe5f8befccb95a2205fa0f745081c2e7b4e
novel <novelbafagih@student.ub.ac.id>
2021-06-19 09:57:14 +0700

    creating color changing background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  function createHsl(hue,lightness,saturation){
+    return(`hsl(${hue},${lightness}%,${saturation}%)`);
+  }
+
+  const [currentHue, setHue] = useState(0);
+  const [color, setColor] = useState({color : 'hsl(0,80%,60%)'});
+
+  const changingColor = () => {
+    currentHue === 250? setHue(0) : setHue(currentHue +50);
+    const currentHslColor = createHsl(currentHue,80,60);
+
+    document.body.style.backgroundColor = currentHslColor;
+    setColor({color : currentHslColor});
+    console.log(color);
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper">
+      <div id="quote-box">
+        <p id="text" style={color}>Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it.</p>
+        <p id="author" style={color}>-Steve jobs</p>
+
+        <button onClick={changingColor} id="new-quote">Press</button>
+
+      </div>
     </div>
   );
 }
